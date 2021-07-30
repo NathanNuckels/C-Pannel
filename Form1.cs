2f46122a40286adb5f262ea32ffbbed05f0be1f7
NathanNuckels <48299408+NathanNuckels@users.noreply.github.com>
2021-07-30 01:08:55 -0500

    Change color with the OK button

--- Form1.cs
+++ Form1.cs
@@ -12,15 +12,30 @@ namespace WindowsApp
 {
     public partial class Form1 : Form
     {
+        Button b1 = new Button();
+        TextBox tb = new TextBox();
+        ColorDialog cd = new ColorDialog();
         public Form1()
         {
+            b1.Click += new EventHandler(b1_click);
+            b1.Text="OK";
+            tb.Location = new Point(50,50);
+            this.Controls.Add(b1);
+            this.Controls.Add(tb);
             this.Paint += new PaintEventHandler(f1_paint);
             InitializeComponent();
         }
+
+        public void b1_click(object sender, EventArgs eventArgs){
+            cd.ShowDialog();
+            tb.BackColor = cd.Color;
+
+        }
+
         private void f1_paint(object sender, PaintEventArgs e){
             Graphics g = e.Graphics;
-            g.DrawString("werds and crap", new Font("Veranda",20), new SolidBrush(Color.Tomato), 40, 40);
-            g.DrawRectangle(new Pen(Color.Pink, 3), 20, 20, 150, 100);
+            //g.DrawString("werds and crap", new Font("Veranda",20), new SolidBrush(Color.Tomato), 40, 40);
+            //g.DrawRectangle(new Pen(Color.Pink, 3), 20, 20, 150, 100);
         }
 
     }
